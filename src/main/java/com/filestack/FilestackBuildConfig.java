bbe86ba682c0c811cc017580163673c793d6eb56
Sergii Polishchuk <sergii.polishchuk@ekreative.com>
2022-07-15 00:44:57 +0300

    fix

---
+++ src/main/java/com/filestack/FilestackBuildConfig.java
@@ -0,0 +1,9 @@
+package com.filestack;
+
+public final class FilestackBuildConfig {
+    public static final String VERSION = "0.9.8";
+    public static final String NAME = "build";
+
+    private FilestackBuildConfig() {
+    }
+}
