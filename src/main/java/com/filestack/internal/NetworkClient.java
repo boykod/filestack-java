a9b1bb5b58be84689863a89625b4f6eb6c58508d
Sergii Polishchuk <sergii.polishchuk@ekreative.com>
2022-07-16 17:31:54 +0300

    fix: fix cancel not triggering

--- src/main/java/com/filestack/internal/NetworkClient.java
+++ src/main/java/com/filestack/internal/NetworkClient.java
@@ -43,7 +43,7 @@ public class NetworkClient {
   }
 
   void cancel() {
-    for (okhttp3.Call call : client.dispatcher().queuedCalls()) {
+    for (okhttp3.Call call : client.dispatcher().runningCalls()) {
       call.cancel();
     }
   }
