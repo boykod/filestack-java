ea03724d90c5322719c82d8d3e8c6c9fca75e716
Sergii Polishchuk <sergii.polishchuk@ekreative.com>
2022-07-18 14:28:18 +0300

    chore: version

--- src/main/java/com/filestack/FilestackBuildConfig.java
+++ src/main/java/com/filestack/FilestackBuildConfig.java
@@ -1,7 +1,7 @@
 package com.filestack;
 
 public final class FilestackBuildConfig {
-    public static final String VERSION = "0.10.3";
+    public static final String VERSION = "0.10.4";
     public static final String NAME = "build";
 
     private FilestackBuildConfig() {
